acb1f736cd23533026ccdde03ecd0f11eb0e4dbf
ranjithreddya <167842288+ranjithreddya@users.noreply.github.com>
2024-05-09 09:55:59 -0500

    Update chaild_tag_xml.py

--- chaild_tag_xml.py
+++ chaild_tag_xml.py
@@ -157,3 +157,81 @@ for rcncltn_rpt in root.findall('.//RcncltnRpt'):
         for child in rcncltn_rpt:
             print("Child tag:", child.tag)
             print("Child text:", child.text)
+
+
+
+
+import csv
+import xml.etree.ElementTree as ET
+import json
+from deepdiff import DeepDiff
+import xmltodict
+
+from ordered_set import OrderedSet
+
+xml_src = "/Users/ranjithrreddyabbidi/ranjith/xml1.xml"
+xml_target = "/Users/ranjithrreddyabbidi/ranjith/xml2.xml"
+xml_final_op = f'/Users/ranjithrreddyabbidi/ranjith'
+print("DEBUGGG")
+tree=ET.parse(xml_src)
+
+root=tree.getroot()
+
+tree1=ET.parse(xml_target)
+
+root1=tree1.getroot()
+xmlstr1 = ET.tostring (root, encoding='utf-8', method='xml').decode() 
+xmlstr2 = ET.tostring (root1, encoding='utf-8', method='xml').decode() 
+
+xmlstr1 = xmlstr1.replace('ns0:','')
+xmlstr2=xmlstr2.replace('ns0:','')
+
+print("DEBUGGG")
+
+def serialize(obj):
+    if isinstance(obj, OrderedSet):
+        return list(obj)
+    elif isinstance(obj, dict):
+        return {key: serialize(value) for key, value in obj.items()}
+    elif isinstance(obj, (list, tuple)):
+        return [serialize(item) for item in obj]
+    return obj
+
+src_json = xmltodict.parse (xmlstr1)
+print("DEBUGGG")
+target_json=xmltodict.parse(xmlstr2)
+print(src_json)
+print (target_json)
+diff=DeepDiff(src_json,target_json)
+print("*"*100)
+print (diff)
+print(json.dumps(diff, default=serialize))
+
+data_str = json.dumps(diff, default=serialize)
+
+try:
+    # Try to load the string as JSON
+    data = json.loads(data_str)
+except json.JSONDecodeError:
+    # If it's not in JSON format, assume it's already a dictionary
+    data = eval(data_str)
+
+output = {}
+for key, value in data.items():
+    if key == 'dictionary_item_added':
+        tagname = value[0].split("'")[-2]
+        output['tagname'] = f'<{tagname}>'
+        output['eod'] = None
+        output['Except'] = None
+        output['test-result'] = 'fail'
+    else:
+        nested_key = list(value.keys())[0]
+        tagname = nested_key.split("'")[-2]
+        output['tagname'] = f'<{tagname}>'
+        output['eod'] = value[nested_key]['new_value']
+        output['Except'] = value[nested_key]['old_value']
+        output['test-result'] = 'fail'
+
+
+
+print(output)
